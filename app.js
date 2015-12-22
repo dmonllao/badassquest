f8c4b872e836770a9442437c52c55d30932ce4ab
David Monllao <david.monllao@gmail.com>
2015-12-22 19:07:16 +0800

    Shake when receiving notifications

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 requirejs.config({
     baseUrl: 'app',
     paths: {
-        bs: '../bower_components/bootstrap/dist/js/bootstrap',
         fontawesome: '../vendor/fontawesome-markers/fontawesome-markers.min',
         jquery: '../bower_components/jquery/dist/jquery',
+        jqueryShake: '../vendor/others/jquery-shake',
+        bs: '../bower_components/bootstrap/dist/js/bootstrap',
         Phaser: '../bower_components/phaser/build/phaser',
         async: '../bower_components/requirejs-plugins/src/async',
         epoly: '../vendor/blackpoolchurch/v3_epoly',
         infobox: '../vendor/google/infobox',
     },
     shim: {
-        bs: {
+        jqueryShake: {
             deps: ['jquery'],
+            exports: "$"
+        },
+        bs: {
+            deps: ['jquery', 'jqueryShake'],
             exports: "$"
         },
         fontawesome: {
