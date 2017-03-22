69f8ded5952ccc58723c43f75f1a2354a9ab6f03
David Monllao <david.monllao@gmail.com>
2017-03-22 21:13:03 +0100

    Remove current location as chrome banned it if no https

--- app/story/Free.js
+++ app/story/Free.js
@@ -8,26 +8,10 @@ define(['jquery', 'story/Base'], function($, StoryBase) {
     }
 
     Free.prototype.getIntro = function() {
-        var text = '<div>Terrorize the city.</div>';
-
-        if (navigator.geolocation) {
-            text += '<div><button id="my-location" class="btn btn-danger">Use my location</button></div>' +
-                '<div> - OR - </div>';
-        }
-
-        return text += '<div><input id="place-input" class="form-control"/></div>';
+        return '<div>Terrorize the city.</div><div><input id="place-input" class="form-control"/></div>';
     };
 
     Free.prototype.getPosition = function(map, initialPositionPromise, callback) {
-
-        // Current location.
-        $('#my-location').on('click', function(ev) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                callback(position);
-                $('#text-action').modal('hide');
-            });
-        });
 
         var input = document.getElementById('place-input');
         var autocomplete = new google.maps.places.Autocomplete(input);
