540a52775fdcf61214830073f5cd9169100436c9
David Monllao <david.monllao@gmail.com>
2017-04-20 04:13:22 +0800

    autocomplete: Select first match if Enter is pressed

--- app/story/Free.js
+++ app/story/Free.js
@@ -1,4 +1,4 @@
-define(['jquery', 'story/Base'], function($, StoryBase) {
+define(['jquery', 'Map', 'story/Base'], function($, Map, StoryBase) {
 
     Free.prototype = Object.create(StoryBase.prototype);
 
@@ -17,15 +17,26 @@ define(['jquery', 'story/Base'], function($, StoryBase) {
         var autocomplete = new google.maps.places.Autocomplete(input);
         autocomplete.setTypes(['geocode']);
         autocomplete.bindTo('bounds', map);
-
         autocomplete.addListener('place_changed', function() {
             var place = autocomplete.getPlace();
             if (!place.geometry) {
-                console.error("Autocomplete's returned place contains no geometry");
+                // Fallback to geocoder.
+                this.getCurrentLocationSelection(callback);
                 return;
             }
             callback(place.geometry.location);
             $('#text-action').modal('hide');
+        }.bind(this));
+    };
+
+    Free.prototype.getCurrentLocationSelection = function(callback) {
+        var geocoder = Map.getGeocoder();
+        var currentValue = $('#place-input').val();
+        geocoder.geocode({"address": currentValue}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                callback(results[0].geometry.location);
+                $('#text-action').modal('hide');
+            }
         });
     };
 
