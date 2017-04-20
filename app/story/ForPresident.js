9364078d5de46827a8d948f87f79e8099cfb33fb
David Monllao <david.monllao@gmail.com>
2017-04-20 23:50:02 +0800

    story: Title and intro updated

--- app/story/ForPresident.js
+++ app/story/ForPresident.js
@@ -6,8 +6,16 @@ define(['bs', 'story/Free', 'UI', 'Icon', 'InfoWindow', 'Mission', 'Foe'], funct
         StoryFree.call(this, user, game);
     }
 
+    ForPresident.prototype.getTitle = function() {
+        var spinningMoney = '<i class="fa fa-money fa-spin fa-lg" style="color: green;"></i>';
+        return spinningMoney + '&nbsp;Badass for president&nbsp;' + spinningMoney;
+    };
+
     ForPresident.prototype.getIntro = function() {
-        return '<div>Badass for president</div><div><input id="place-input" class="form-control"/></div>';
+        return '<div>A quest <i class="fa fa-map-o"></i> ' +
+            'to release your inner badass <i class="fa fa-hand-rock-o" style="color: #B58A3F;"></i> ' +
+            'in real world locations <i class="fa fa-map-marker" style="color: #e15c5c"></i></div>' +
+            '<div><input id="place-input" class="form-control"/></div>';
     };
 
     return ForPresident;
