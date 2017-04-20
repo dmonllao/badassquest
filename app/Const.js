9430b7520c1e6b4f33ccd34b41506c934acd4e5c
David Monllao <david.monllao@gmail.com>
2017-04-20 21:09:35 +0800

    story: Change main character

--- app/Const.js
+++ app/Const.js
@@ -1,7 +1,7 @@
 define(function() {
     return {
         defaultMapCenterBackground: {lat: -31.956427724868902, lng: 115.86163059965043},
-        characterPic: 'img/badass.png',
+        characterPic: 'img/trump.png',
         initFood: 4000,
         initHealth: 100,
         initSpeed: 4,
